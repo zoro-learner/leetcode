f9826133b6cd4d621c345d9e3ff2ce5bfd0698c5
wangbo_thu <wangbo_thu@163.com>
2020-08-06 09:23:12 +0800

    until 2020.08.06

---
+++ editor/en/SearchInsertPosition.java
@@ -0,0 +1,70 @@
+package leetcode.editor.en;
+
+//Given a sorted array and a target value, return the index if the target is fou
+//nd. If not, return the index where it would be if it were inserted in order. 
+//
+// You may assume no duplicates in the array. 
+//
+// Example 1: 
+//
+// 
+//Input: [1,3,5,6], 5
+//Output: 2
+// 
+//
+// Example 2: 
+//
+// 
+//Input: [1,3,5,6], 2
+//Output: 1
+// 
+//
+// Example 3: 
+//
+// 
+//Input: [1,3,5,6], 7
+//Output: 4
+// 
+//
+// Example 4: 
+//
+// 
+//Input: [1,3,5,6], 0
+//Output: 0
+// 
+// Related Topics Array Binary Search 
+// 👍 2489 👎 257
+
+
+/**
+ * @author zoro-learner
+ * @create 2020-08-04 19:47:45
+ */
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        Solution solution = new SearchInsertPosition().new Solution();
+        
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    
+}
